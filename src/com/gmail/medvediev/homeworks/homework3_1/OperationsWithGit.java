258983f0bbffdc102957f7973e36651894dd0b4b
highterban <nikol.med96@gmail.com>
2025-04-28 21:09:52 +0300

    Changed the text shown in console

--- src/com/gmail/medvediev/homeworks/homework3_1/OperationsWithGit.java
+++ src/com/gmail/medvediev/homeworks/homework3_1/OperationsWithGit.java
@@ -2,6 +2,6 @@ package com.gmail.medvediev.homeworks.homework3_1;
 
 public class OperationsWithGit {
     public static void main(String[] args) {
-        System.out.println("This is shop-online app");
+        System.out.println("Shop-online for customers");
     }
 }
