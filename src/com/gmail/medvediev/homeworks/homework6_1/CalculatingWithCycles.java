5e65b4385f65067dd3508fc1ea0a9e2e09a2618c
highterban <nikol.med96@gmail.com>
2025-05-11 15:41:06 +0300

    Add the file, that fix bugs of previous commit and now user can see all the three methods

--- src/com/gmail/medvediev/homeworks/homework6_1/CalculatingWithCycles.java
+++ src/com/gmail/medvediev/homeworks/homework6_1/CalculatingWithCycles.java
@@ -4,16 +4,27 @@ import java.util.Scanner;
 
 public class CalculatingWithCycles {
     public static void main(String[] args) {
-       firstMethod();
-    }
-
-    public static void firstMethod() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Insert border:");
         int border = scanner.nextInt();
         int sum = 0;
+        int num = 0;
+        System.out.println("First method:");
+        firstMethod(border, sum);
+        System.out.println("Second method:");
+        secondMethod(num, border, sum);
+        System.out.println("Third method:");
+        if (border > 0) {
+            thirdMethod(num, border, sum);
+        }
+       else{
+            System.out.println("------------------------");
+            System.out.println("Sum of numbers is " + sum);
+        }
+    }
 
+    public static void firstMethod(int border, int sum) {
         for (int num = 1; num <= border; num++) {
             sum += num;
             System.out.printf("%d) Num is %d, sum is %d%n", num, num, sum);
@@ -21,32 +32,24 @@ public class CalculatingWithCycles {
         System.out.println("------------------------");
         System.out.println("Sum of numbers is " + sum);
     }
-    public static void secondMethod() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Insert border:");
-        int border = scanner.nextInt();
-        int sum = 0;
-        int num = 0;
-        while (num<border){
+
+    public static void secondMethod(int num, int border, int sum) {
+
+        while (num < border) {
             num++;
-            sum+=num;
+            sum += num;
             System.out.printf("%d) Num is %d, sum is %d%n", num, num, sum);
         }
         System.out.println("------------------------");
         System.out.println("Sum of numbers is " + sum);
     }
 
-    public static void thirdMethod() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Insert border:");
-        int border = scanner.nextInt();
-        int sum = 0;
-        int num = 0;
-        do{
+    public static void thirdMethod(int num, int border, int sum) {
+        do {
             num++;
-            sum+=num;
+            sum += num;
             System.out.printf("%d) Num is %d, sum is %d%n", num, num, sum);
-        } while(num<border);
+        } while (num < border);
         System.out.println("------------------------");
         System.out.println("Sum of numbers is " + sum);
     }
